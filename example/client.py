13b7020505e8d87385f4ea580645bc96a2f74ae6
Mikael Åhlén <mikael.ahlen@gmail.com>
2013-09-08 04:03:58 +0200

    changed indentation from tabs to spaces, like it should be - thanks sublime

--- example/client.py
+++ example/client.py
@@ -12,30 +12,30 @@ context = zmq.Context()
 
 # or just write the digits as strings to avoid type conversion :)
 proto_mapper = {
-	0: protocol.RpcGetUsernameRequest,
-	1: protocol.RpcGetUsernameResponse
+    0: protocol.RpcGetUsernameRequest,
+    1: protocol.RpcGetUsernameResponse
 }
 
 proto_inverse_mapper = {v.__name__: k for k, v in proto_mapper.iteritems()}
 
 def main(user_id):
-	socket = context.socket(zmq.REQ)
-	socket.connect('tcp://localhost:5556')
+    socket = context.socket(zmq.REQ)
+    socket.connect('tcp://localhost:5556')
 
-	req = protocol.RpcGetUsernameRequest(user_id)
-	socket.send(json.dumps(["get_username", proto_inverse_mapper[req.__class__.__name__]]), zmq.SNDMORE)
-	socket.send(req.SerializeToString())
+    req = protocol.RpcGetUsernameRequest(user_id)
+    socket.send(json.dumps(["get_username", proto_inverse_mapper[req.__class__.__name__]]), zmq.SNDMORE)
+    socket.send(req.SerializeToString())
 
-	response_type = socket.recv()
-	response = socket.recv()
+    response_type = socket.recv()
+    response = socket.recv()
 
-	message = proto_mapper[int(response_type)]()
-	message.ParseFromString(response)
+    message = proto_mapper[int(response_type)]()
+    message.ParseFromString(response)
 
-	socket.close()
-	return message.user_id, message.username
+    socket.close()
+    return message.user_id, message.username
 
 if __name__ == "__main__":
-	for x in xrange(2000):
-		print x, main(123), main(987)
+    for x in xrange(2000):
+        print x, main(123), main(987)
 
--- example/protocol.py
+++ example/protocol.py
@@ -8,28 +8,28 @@
 import json
 
 class RpcGetUsernameRequest(object):
-	def __init__(self, user_id=None):
-		self.user_id = user_id
+    def __init__(self, user_id=None):
+        self.user_id = user_id
 
-	def SerializeToString(self):
-		return str(self.user_id)
+    def SerializeToString(self):
+        return str(self.user_id)
 
-	def ParseFromString(self, data):
-		self.user_id = int(data)
+    def ParseFromString(self, data):
+        self.user_id = int(data)
 
 class RpcGetUsernameResponse(object):
-	def __init__(self, user_id=None, username=None):
-		self.user_id = user_id
-		self.username = username
+    def __init__(self, user_id=None, username=None):
+        self.user_id = user_id
+        self.username = username
 
-	def SerializeToString(self):
-		return json.dumps({
-			"id": self.user_id,
-			"username": self.username
-		})
+    def SerializeToString(self):
+        return json.dumps({
+            "id": self.user_id,
+            "username": self.username
+        })
 
-	def ParseFromString(self, data):
-		fields = json.loads(data)
-		self.user_id = fields.get("id")
-		self.username = fields.get("username")
+    def ParseFromString(self, data):
+        fields = json.loads(data)
+        self.user_id = fields.get("id")
+        self.username = fields.get("username")
 
